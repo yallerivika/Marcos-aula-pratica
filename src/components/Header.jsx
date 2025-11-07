5e0f59c85093fd4a41ff791b0d7a5e02aaa2b763
yalle <yalle687@gmail.com>
2025-11-07 23:10:23 +0000

    mudei o nome

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ export default function Header() {
     <>
       <HeaderContainer>
         <HeaderContent>
-          <Logo href="/">ADS Animes</Logo>
+          <Logo href="/">ADS Animes turma C</Logo>
 
           <Nav>
             <NavLink href="/">Home</NavLink>
